8b38bcaecd94298938b9b67209eda597c24599fd
ajay <ajayjarwal44@gmail.com>
2022-10-08 18:06:45 +0530

    deploy project on firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCfAAIKDqy5bR7YZVosNPupO8M8te9S5XY",
+    authDomain: "task-manager-fc7e8.firebaseapp.com",
+    projectId: "task-manager-fc7e8",
+    storageBucket: "task-manager-fc7e8.appspot.com",
+    messagingSenderId: "164770910439",
+    appId: "1:164770910439:web:85379b83a2d8e6eb315242",
+    measurementId: "G-MHSX79X5VF"
+  }
 };
